bc478c9aee3d2d6c255688a3616d526bd0cb9206
Fressia Barrios <fressiabarrios2@gmail.com>
2024-01-12 23:02:17 +0100

    ex1 done

--- Week2/prep-exercises/1-catwalk-async-await/index.js
+++ Week2/prep-exercises/1-catwalk-async-await/index.js
@@ -8,13 +8,31 @@ const DANCING_CAT_URL =
 
 function walk(img, startPos, stopPos) {
   return new Promise((resolve) => {
-    // Copy over the implementation from last week
+    let currentPos = startPos;
+
+    const walkStep = () => { 
+      if (currentPos < stopPos) {
+        currentPos += STEP_SIZE_PX;
+        img.style.left = currentPos + 'px';
+        setTimeout(walkStep, STEP_INTERVAL_MS); // creates a delay between each step of the cat's walk
+      } else {
+        resolve();
+      }
+    };
+
+    walkStep();
   });
 }
 
 function dance(img) {
   return new Promise((resolve) => {
-    // Copy over the implementation from last week
+    const walkingCatSrc = img.src;
+    img.src = DANCING_CAT_URL;
+
+    setTimeout(() => {
+      img.src = walkingCatSrc;
+      resolve();
+    }, DANCE_TIME_MS);
   });
 }
 
@@ -24,7 +42,11 @@ async function catWalk() {
   const centerPos = (window.innerWidth - img.width) / 2;
   const stopPos = window.innerWidth;
 
-  // Use async/await syntax to loop the walk and dance functions
+  while (true) {
+    await walk(img, startPos, centerPos)
+    await dance(img)
+    await walk(img, centerPos, stopPos)
+  }
 }
 
 window.addEventListener('load', catWalk);
